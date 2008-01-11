ed9e70145857ef2c4420c11fb1656bce0c7be291
wsobel <wsobel@daed6d57-6b21-0410-a8f4-a6969d1d4b01>
2008-01-11 23:25:55 +0000

    Added table definition for 2.0.2. enum method no longer automatically created.
    git-svn-id: svn://rubyforge.org/var/svn/enum-column/plugins/enum-column@17 daed6d57-6b21-0410-a8f4-a6969d1d4b01

--- init.rb
+++ init.rb
@@ -2,6 +2,7 @@ require 'enum/enum_adapter'
 require 'enum/mysql_adapter' if defined? ActiveRecord::ConnectionAdapters::MysqlAdapter
 require 'enum/postgresql_adapter' if defined? ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
 require 'enum/schema_statements'
+require 'enum/schema_definitions'
 require 'enum/quoting'
 require 'enum/validations'
 require 'enum/active_record_helper'
